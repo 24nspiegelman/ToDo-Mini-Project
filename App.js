a99150969abf3a4c42e2d49848fa733b35b835f5
24nspiegelman <24nspiegelman@athenian.org>
2024-02-27 10:08:13 -0800

    Working on Table

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Alert, icons } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component'; 
+
 
 export default function App() {
+
+  const header = ['Task', 'Completed?']
+  const data = [['Task 1', '✓'],
+                ['Task 2', 'x']
+
+]
   return (
+    <View>
+      <SafeAreaView>
+      <Pressable
+        id="addTask"
+        title="+"
+        style = {styles.button}
+        onLongPress={()=>Alert.alert('Long Press')}
+        onPress={()=>Alert.alert('Button Pressed')}
+        color = "#f194ff"
+
+        >
+          <View style={styles.buttonContainer}><Text style={styles.buttonText}>+</Text></View>
+        </Pressable>
+      </SafeAreaView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.tableContainer}>
+        <Table borderStyle={{ borderWidth: 2,  
+                borderColor: 'black' }}> 
+                <Row data={header} /> 
+                <Rows data={data} /> 
+            </Table> 
+        </View>
+    </View>
     </View>
   );
 }
@@ -16,5 +46,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'yellow'
   },
+  button: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    elevation: 3,
+    backgroundColor: 'green',
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor:'red',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  buttonText: {
+    fontSize: 100,
+  },
+  tableContainer :{
+    flex: 1,
+    height: 400,
+    width: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
 });
